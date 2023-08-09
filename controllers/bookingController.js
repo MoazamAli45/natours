c695701eb60696f4e2d5100d697633a28b8438b8
MoazamAli45 <21-SE-48@students.uettaxila.edu.pk>
2023-08-10 00:04:05 +0500

    Fixed bug

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -92,7 +92,7 @@ exports.webhookCheckout = (req, res, next) => {
     // send to stripe
     return res.status(400).send(`Webhook error: ${err.message}`);
   }
-  if (event === 'checkout.session.completed')
+  if (event.type === 'checkout.session.completed')
     createCheckoutBooking(event.data.object);
 
   res.status(200).json({
